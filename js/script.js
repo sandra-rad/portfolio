ce228774806dbef082fc88b7677d7164d5cf559a
Sandra <radsandra@yahoo.com>
2019-05-23 02:10:33 +0300

    update-film-work

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  fade: true,
-  arrows: true,
-  infinite: true,
-  prevArrow: $('.prev'),
-  nextArrow: $('.next'),
-  asNavFor: '.slider-nav'
+$(document).ready(function() {
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: true,
+    infinite: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    asNavFor: ".slider-nav"
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    arrows: false,
+    cssEase: "",
+    infinite: true,
+    focusOnSelect: true
+  });
 });
-$('.slider-nav').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: false,
-  arrows: false,
-  cssEase: '',
-  infinite: true,
-  focusOnSelect: true,
-
-});
-
-        
