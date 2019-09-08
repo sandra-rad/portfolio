a40bbaea1399f88f753a4949c6f9aa5e39c04f05
Sandra <radsandra@yahoo.com>
2019-09-08 22:48:03 +0300

    update site
    update site

--- js/script.js
+++ js/script.js
@@ -1,42 +1,29 @@
 $(document).ready(function() {
   $(".error").hide();
-  var widthV = $(window).width();  
-  if (widthV < 800  ) {
-    $(".grid-container").hide();
+  var widthV = $(window).width();
+  if (widthV < 800) {
+    $(".grid-container, #film-section").hide();
     $(".error").show();
-    
   }
-  $(window).resize(function(){
-    if ($(window).width() < 800) {  
-      $(".grid-container").hide();
+  $(window).resize(function() {
+    if ($(window).width() < 800) {
+      $(".grid-container, #film-section").hide();
       $(".error").show();
+    } else {
+      $(".grid-container, #film-section").show();
+      $(".error").hide();
     }
-    else{
-      $(".grid-container").show();
-      
-    }     
-
-});
-
-  $(".slider-for").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    arrows: true,
-    infinite: true,
-    cssEase: true,
-    prevArrow: $(".prev"),
-    nextArrow: $(".next"),
-    asNavFor: ".slider-nav"
   });
-  $(".slider-nav").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: ".slider-for",
-    dots: false,
-    arrows: false,
-    cssEase: true,
+
+  
+  $('.one-time').slick({
+    dots: true,
     infinite: true,
-    focusOnSelect: true
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
   });
-});
+
+  });
